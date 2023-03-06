61bf900219e36b7929d2a12137071e47860e9a5e
Victor T <116121215+littlevixplanet@users.noreply.github.com>
2023-03-06 15:27:13 -0800

    Challenge 2
    Completed first game by adding collisions and making the pizza destroy the animals, almost finished challenge 2

--- Unit2BasicGameplayTorres/Assets/Scripts/SpawnManager.cs
+++ Unit2BasicGameplayTorres/Assets/Scripts/SpawnManager.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float spawnRangeX = 16;
+    private float spawnPosZ = 20;
     public GameObject[] animalPrefabs;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-16,16), 0, 20), animalPrefabs[animalIndex].transform.rotation);
-        }
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
